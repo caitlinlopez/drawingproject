438d3008c9a1549406b76560b07808a1f674ad7c
caitlin <caitlinlopez@users.noreply.github.com>
2016-10-10 15:41:26 -0700

    Click on ufo to change color of sky.

--- drawingproject.js
+++ drawingproject.js
@@ -7,9 +7,6 @@ canvas.fillRect(425, 20, 450, 580);
 canvas.fillStyle = "#CCCCFF";
 canvas.fillRect(435, 35, 430, 550);
 
-canvas.fillStyle = "#CCCCFF";
-canvas.fillRect(435, 35, 430, 550);
-
 canvas.beginPath();
 canvas.fillStyle = "green";
 canvas.moveTo(585,580);
@@ -17,11 +14,63 @@ canvas.lineTo(435,580);
 canvas.lineTo(435,60);
 canvas.fill();
 
+canvas.beginPath();
+canvas.fillStyle = "green";
+canvas.ellipse(779, 425, 85, 75, 165 * Math.PI/180, 0, 2 * Math.PI);
+canvas.fill();
+
+canvas.beginPath();
+canvas.fillStyle = "green";
+canvas.ellipse(650, 400, 65, 85, 170 * Math.PI/180, 0, 2 * Math.PI);
+canvas.fill();
+
 canvas.fillStyle = "green";
 canvas.fillRect(435, 425, 430, 160);
 
 canvas.beginPath();
 canvas.fillStyle = "gray";
-canvas.arc(600, 150, 50, 0, 2*Math.PI);
-canvas.stroke();
+canvas.ellipse(600, 150, 22, 80, 555559 * Math.PI/180, 0, 2 * Math.PI);
 canvas.fill();
+
+canvas.beginPath();
+canvas.fillStyle = "gray";
+canvas.ellipse(600, 150, 38, 50, 555560 * Math.PI/180, 0, 2 * Math.PI);
+canvas.fill();
+
+function button() {
+  canvas.fillStyle = "white";
+  canvas.fillRect(425, 20, 450, 580);
+
+  canvas.fillStyle = "black";
+  canvas.fillRect(435, 35, 430, 550);
+
+  canvas.beginPath();
+  canvas.fillStyle = "green";
+  canvas.moveTo(585,580);
+  canvas.lineTo(435,580);
+  canvas.lineTo(435,60);
+  canvas.fill();
+
+  canvas.beginPath();
+  canvas.fillStyle = "green";
+  canvas.ellipse(779, 425, 85, 75, 165 * Math.PI/180, 0, 2 * Math.PI);
+  canvas.fill();
+
+  canvas.beginPath();
+  canvas.fillStyle = "green";
+  canvas.ellipse(650, 400, 65, 85, 170 * Math.PI/180, 0, 2 * Math.PI);
+  canvas.fill();
+
+  canvas.fillStyle = "green";
+  canvas.fillRect(435, 425, 430, 160);
+
+  canvas.beginPath();
+  canvas.fillStyle = "gray";
+  canvas.ellipse(600, 150, 22, 80, 555559 * Math.PI/180, 0, 2 * Math.PI);
+  canvas.fill();
+
+  canvas.beginPath();
+  canvas.fillStyle = "gray";
+  canvas.ellipse(600, 150, 38, 50, 555560 * Math.PI/180, 0, 2 * Math.PI);
+  canvas.fill();
+}
